a1ddb94301cb9d50df98bf3996883d1aa49e2512
Simret Belete <104854158+Simret101@users.noreply.github.com>
2024-01-05 22:41:45 +0300

    Added solution for Sort Colors

--- week1/sort-colors.py
+++ week1/sort-colors.py
@@ -4,20 +4,22 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        l=0
+        cur,l=0,0
         r=len(nums)-1
-        cur=0
         while cur<=r:
             if nums[cur]==0:
                 nums[l],nums[cur]=nums[cur],nums[l]
                 l+=1
                 cur+=1
             elif nums[cur]==2:
-                nums[r],nums[cur]=nums[cur],nums[r]
+                nums[cur],nums[r]=nums[r],nums[cur]
                 r-=1
             else:
                 cur+=1
-            
+    
+
+        
+    
 
 
     
